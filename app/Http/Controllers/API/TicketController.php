093fd39dd657e0b31ef841933930fc296872f912
Simon Roberts <simon@bigboffin.com>
2024-12-20 11:32:26 +0000

    Added the open/closed api endpoints

--- app/Http/Controllers/API/TicketController.php
+++ app/Http/Controllers/API/TicketController.php
@@ -11,7 +11,47 @@ class TicketController extends Controller
 
     public function index(Request $request)
     {
-        return Ticket::with(['user'])->where('status', false)->paginate(3);
+
+        $perPage = 3;
+        $columns = ['*'];
+        $pageName = 'page';
+        $status = false;
+
+        $page = $request->input('page', 1);
+        $statusFilter = $request->input('status_filter', 'open');
+
+        if ($statusFilter == 'close') {
+            $status = true;
+        }
+
+        $tickets = Ticket::with(['user'])
+            ->where('status', $status)
+            ->latest()
+            ->paginate($perPage, $columns, $pageName, $page);
+
+
+        return response()->json($tickets);
     }
 
+    public function open(Request $request)
+    {
+        $openTickets = Ticket::with(['user'])
+            ->where('status', 0)
+            ->latest()
+            ->paginate();
+
+        return response()->json($openTickets);
+
+    }
+
+    public function closed(Request $request)
+    {
+        $closedTickets = Ticket::with(['user'])
+            ->where('status', 1)
+            ->latest()
+            ->paginate();
+
+        return response()->json($closedTickets);
+
+    }
 }
