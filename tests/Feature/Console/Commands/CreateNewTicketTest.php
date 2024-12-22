f090dc65016abef0f5a454ce33654f119a9caae0
Simon Roberts <simon@bigboffin.com>
2024-12-22 11:42:11 +0000

    Filling out more tests

--- tests/Feature/Console/Commands/CreateNewTicketTest.php
+++ tests/Feature/Console/Commands/CreateNewTicketTest.php
@@ -2,18 +2,25 @@
 
 namespace Tests\Feature\Console\Commands;
 
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class CreateNewTicketTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    use DatabaseTransactions;
+
     public function test_create_new_ticket_command(): void
     {
+        User::factory(10)->create();
+
         // Call the artisan command to create a new ticket
         $this->artisan('app:create-new-ticket')
             ->expectsOutput('Ticket created successfully')
             ->assertExitCode(0);
+
+        // Assert that the ticket was created successfully
+        $this->assertDatabaseCount(Ticket::class, 1);
     }
 }
--- tests/Feature/Console/Commands/ProcessTicketTest.php
+++ tests/Feature/Console/Commands/ProcessTicketTest.php
@@ -2,17 +2,26 @@
 
 namespace Tests\Feature\Console\Commands;
 
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class ProcessTicketTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use DatabaseTransactions;
 
-        $response->assertStatus(200);
+    public function test_processing_ticket_command(): void
+    {
+        $user = User::factory()->create();
+
+        Ticket::factory()->create([
+            'user_id' => $user->id,
+            'status' => false,
+        ]);
+
+        $this->artisan('app:process-ticket')
+            ->expectsOutput('Ticket processed successfully')
+            ->assertExitCode(0);
     }
 }
